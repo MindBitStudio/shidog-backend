c83bc2870235e373796996de35ec093b858af27d
MindBitStudio <mindbitstudio@gmail.com>
2025-03-22 14:52:58 +0100

    Subiendo backend y frontend

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,10 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+connectionString: process.env.DATABASE_URL,
+ssl: {
+    rejectUnauthorized: false // Necesario para conexiones seguras en Render
+}
 });
-
-pool.connect()
-    .then(() => console.log("📌 Conectado a PostgreSQL"))
-    .catch(err => console.error("❌ Error al conectar PostgreSQL:", err));
 
 module.exports = pool;
